d9ab43df57955c12e9bbb3f5915498e54e8f23c4
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-26 10:24:39 -0700

    Update sort.h file

--- sort.h
+++ sort.h
@@ -29,5 +29,6 @@ void lomuto_quick_sort(int *array, size_t size, int lo, int hi);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 #endif /* SORT_H */
